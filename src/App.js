c2e61f5e9218b1c30204ceb9b7733e08f6ef1b75
Jason <jason@Jasons-MacBook-Air.local>
2023-08-31 15:31:18 -0700

    installed react bootstrap and applied bootstrap to the nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, {useState} from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
 import Nav from './components/nav'
 import profile from './components/profile'
 
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,15 +1,21 @@
-import React, {useState} from 'react'
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 export default function nav() {
 
     return (
-        <div>
-            <nav>
-                <ul>
-                    <li><a href="/profile">Profile</a></li>
-                    <li><a href="/events">Events</a></li>
-                </ul>
-            </nav>
-        </div>
+        <Container>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="#">Project Name</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/profile">Profile</Nav.Link>
+                        <Nav.Link href="/events">Events</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </Container>
         )
     } 
