610102910443b69ab447171757d797a6a846a9ec
Jason <jason@Jasons-MacBook-Air.local>
2023-08-31 14:58:42 -0700

    moved components inside of src and began nav component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import React, {useState} from 'react'
+import Nav from './components/nav'
 import profile from './components/profile'
 
 function App() {
  return (
-    
+    <div className="App">
+      <Nav />
+    </div>
  )
 }
 
---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+export default function nav() {
+
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="/profile">Profile</a></li>
+                    <li><a href="/events">Events</a></li>
+                </ul>
+            </nav>
+        </div>
+        )
+    } 
