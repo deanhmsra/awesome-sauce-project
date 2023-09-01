f62e087468ecba2c61090bf988a975986bcbb4e9
Jason <jason@Jasons-MacBook-Air.local>
2023-09-01 00:18:29 -0700

    displaying users lists

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import React, {useState} from 'react'
 import Nav from './components/nav'
 import Profile from './components/profile'
+import DisplayUsersInterest from './components/displayUsersInterest';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function App() {
 return (
+  <div>
     <Nav />
-)
+    <DisplayUsersInterest />
+  </div>
+)    
 }
 
 export default App;
