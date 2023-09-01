b6f5be4aba53f6780c5ba7f5ce6a5f145d2f0308
Brian Guintos Recinos <brianguintos@gmail.com>
2023-08-31 20:21:50 -0700

    added eventInfo component into events component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import './App.css';
 import React, {useState} from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Nav from './components/nav'
-import profile from './components/profile'
-import events from './components/events'
 import Profile from './components/profile'
+import events from './components/events'
 
 function App() {
  return (
