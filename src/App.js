4c6f197097101c1eeb18bcc7f157c91539892ad4
Jason <jason@Jasons-MacBook-Air.local>
2023-09-01 00:48:10 -0700

    install react-router-dom and added Routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import React, {useState} from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Nav from './components/nav'
 import Profile from './components/profile'
+import Events from './components/events'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
 return (
+    <Router>
     <Nav />
+    
+    <div className="display">
+    <Routes>
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/events" element={<Events />} />
+    </Routes>
+    </div>
+    </Router>
 )
 }
 
